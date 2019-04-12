235c6ed0114564399ef67d270e1f3ddddef16b55
FlorianKn <f.knoerzer@outlook.com>
2019-04-12 12:34:48 +0200

    Add additional label

--- prepareNotchData.py
+++ prepareNotchData.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 USERS = np.array([["User", 1], ["1", 200], ["2", 47]])
-LABELS = np.array([["Label", 1], ["HAMMER_CURLS", 100], ["BICEPS_CURLS", 100], ["REVERSE_CURLS", 47]])
+LABELS = np.array([["Label", 1], ["HAMMER_CURLS", 100], ["BICEPS_CURLS", 50], ["TRICEPS_DRUECKEN", 50], ["REVERSE_CURLS", 47]])
 NEW_HEADER = np.array([["Time","ElbowFlexion", "ElbowSupination", "ShoulderFlexion", "ShoulderAbduction", "ShoulderRotation", "User", "Label"]])
 
 def readCSV(path):
