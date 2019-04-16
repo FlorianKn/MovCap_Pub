117156a715ca8df11ed6a97ef8827f7d022f398e
FlorianKn <floknoerzer@web.de>
2019-04-16 17:28:44 +0200

    Update plot script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MovCap
 Capture and classify movements with NN 
+## Explore dataset  
+`python -c "from plot import plotLabels; print plotLabels()"`  
 ![Fig1](data/fig/fig1.png)
--- plot.py
+++ plot.py
@@ -7,9 +7,10 @@ df = pd.read_csv('data/data.csv', header = None, names = columns)
 df.head()
 # Without first header
 df = df.iloc[2:]
-df['Label'].value_counts().plot(kind='bar', title='Training examples by activity type');
-plt.show()
 
+def plotLabels():
+    df['Label'].value_counts().plot(kind='bar', title='Training examples by activity type');
+    plt.show()
 
 def plot_activity(activity, df):
     # TODO: plot axis of one specific Label
